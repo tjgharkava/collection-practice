dff373ad6d493c577295e246d3aab2e7412c1f09
Temo <tdzhgarkava@yahoo.com>
2025-07-31 14:38:52 +0400

    feed commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,4 +7,29 @@ fun main() {
 
     val foxiesHealthCheck = mutableMapOf<String, Any?>("Bonnie" to false, "Jubilee" to false, "Frodo" to false, "Foxie" to false)
 
+    println("First, ${responsibilities[0]}.")
+
+    println("Feeding Bonnie...")
+    foxiesHealthCheck["Bonnie"] = true
+    responsibilitiesComplete++
+    timeSpent++
+
+    println("Feeding Jubilee...")
+    foxiesHealthCheck["Jubilee"] = true
+    responsibilitiesComplete++
+    timeSpent++
+
+    println("Feeding Frodo...")
+    foxiesHealthCheck["Frodo"] = true
+    responsibilitiesComplete++
+    timeSpent++
+
+    println("Feeding Foxie...")
+    foxiesHealthCheck["Foxie"] = true
+    responsibilitiesComplete++
+    timeSpent++
+
+    println("All chimps have now been fed! You've completed $responsibilitiesComplete / $totalShiftTime responsibilities.")
+
+
 }
