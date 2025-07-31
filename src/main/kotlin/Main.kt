8fac22c2b00b1aa53dfda2ee8c563e8ff74dcab0
Temo <tdzhgarkava@yahoo.com>
2025-07-31 14:25:57 +0400

    initial commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,10 @@
 
 fun main() {
+    val responsibilities = listOf("feed the chimps", "play a random game", "conduct a health check on Foxie")
+    var responsibilitiesComplete = 0
+    var timeSpent = 0
+    val totalShiftTime = 4
+
+    val foxiesHealthCheck = mutableMapOf<String, Any?>("Bonnie" to false, "Jubilee" to false, "Frodo" to false, "Foxie" to false)
 
 }
